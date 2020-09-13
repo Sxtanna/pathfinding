64375003045a76a0478bcc68f174fe50b0b924c7
Sxtanna <ranald.taylor@gmail.com>
2020-09-13 13:55:17 -0400

    fixed weird indentation

--- astar/src/main/java/com/sxtanna/path/Heuristic.java
+++ astar/src/main/java/com/sxtanna/path/Heuristic.java
@@ -1,56 +1,56 @@
 package com.sxtanna.path;
 
 public enum Heuristic
-	{
+{
 
-		// manhattan
-		M
+	// manhattan
+	M
+			{
+				@Override
+				public int get(final Node node0, final Node node1)
 				{
-					@Override
-					public int get(final Node node0, final Node node1)
-					{
-						final int dx = Math.abs(node0.getX() - node1.getX());
-						final int dy = Math.abs(node0.getY() - node1.getY());
-						final int dz = Math.abs(node0.getZ() - node1.getZ());
+					final int dx = Math.abs(node0.getX() - node1.getX());
+					final int dy = Math.abs(node0.getY() - node1.getY());
+					final int dz = Math.abs(node0.getZ() - node1.getZ());
 
-						return (int) (VALUE_D0 * (dx + dy + dz));
-					}
-				},
-		// diagonal
-		D
+					return (int) (VALUE_D0 * (dx + dy + dz));
+				}
+			},
+	// diagonal
+	D
+			{
+				@Override
+				public int get(final Node node0, final Node node1)
 				{
-					@Override
-					public int get(final Node node0, final Node node1)
-					{
-						final int dx = Math.abs(node0.getX() - node1.getX());
-						final int dy = Math.abs(node0.getY() - node1.getY());
-						final int dz = Math.abs(node0.getZ() - node1.getZ());
+					final int dx = Math.abs(node0.getX() - node1.getX());
+					final int dy = Math.abs(node0.getY() - node1.getY());
+					final int dz = Math.abs(node0.getZ() - node1.getZ());
 
-						return (int) (VALUE_D0 * (dx + dy + dz) + (VALUE_D1 - (2 * VALUE_D0)) * Math.min(dx, dz));
-					}
-				},
-		// euclidean
-		E
+					return (int) (VALUE_D0 * (dx + dy + dz) + (VALUE_D1 - (2 * VALUE_D0)) * Math.min(dx, dz));
+				}
+			},
+	// euclidean
+	E
+			{
+				@Override
+				public int get(final Node node0, final Node node1)
 				{
-					@Override
-					public int get(final Node node0, final Node node1)
-					{
-						final int dx = Math.abs(node0.getX() - node1.getX());
-						final int dy = Math.abs(node0.getY() - node1.getY());
-						final int dz = Math.abs(node0.getZ() - node1.getZ());
+					final int dx = Math.abs(node0.getX() - node1.getX());
+					final int dy = Math.abs(node0.getY() - node1.getY());
+					final int dz = Math.abs(node0.getZ() - node1.getZ());
 
-						final double s2 = Math.sqrt((dx * dx) + (dz * dz));
-						final double s3 = Math.sqrt((s2 * s2) + (dy * dy));
+					final double s2 = Math.sqrt((dx * dx) + (dz * dz));
+					final double s3 = Math.sqrt((s2 * s2) + (dy * dy));
 
-						return (int) s3;
-					}
-				};
+					return (int) s3;
+				}
+			};
 
 
-		private static final double VALUE_D0 = 1.0;
-		private static final double VALUE_D1 = Math.sqrt(2);
+	private static final double VALUE_D0 = 1.0;
+	private static final double VALUE_D1 = Math.sqrt(2);
 
 
-		public abstract int get(final Node node0, final Node node1);
+	public abstract int get(final Node node0, final Node node1);
 
-	}
+}
